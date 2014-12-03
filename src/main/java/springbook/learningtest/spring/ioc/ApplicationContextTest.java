fd9b98cad688dfb39e11150769b131705bb354cf
kimmari10 <kimmari10@naver.com>
2014-12-03 11:33:40 +0900

    1.1.1 IoC 컨테이너를 이용해 애플리케이션 만들기 p.58 StaticApplicationContext와 BeanDefinition을 이용한 빈등록

---
+++ src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
@@ -0,0 +1,39 @@
+package springbook.learningtest.spring.ioc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.support.StaticApplicationContext;
+
+import springbook.learningtest.spring.ioc.bean.Hello;
+
+public class ApplicationContextTest {
+	
+	@Test
+	public void registerBean() {
+		StaticApplicationContext ac = new StaticApplicationContext();
+		ac.registerSingleton("hello1", Hello.class);
+		
+		Hello hello1 = ac.getBean("hello1", Hello.class);
+		assertThat(hello1,  is(notNullValue()));
+	
+		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+		ac.registerBeanDefinition("hello2", helloDef);
+		
+		Hello hello2 = ac.getBean("hello2", Hello.class);
+		assertThat(hello2.sayHello(), is("Hello Spring"));
+		
+		assertThat(hello1, is(not(hello2)));
+		assertThat(ac.getBeanFactory().getBeanDefinitionCount(), is(2));
+		
+	}
+	
+
+	
+}
