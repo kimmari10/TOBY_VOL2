a45e6cb829bb66c2685b44a2d1b1fadb21253c7e
kimmari10 <kimmari10@naver.com>
2014-12-03 11:39:49 +0900

    p.59 DI정보 테스트

--- src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
+++ src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
@@ -7,10 +7,12 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.StaticApplicationContext;
 
 import springbook.learningtest.spring.ioc.bean.Hello;
+import springbook.learningtest.spring.ioc.bean.StringPrinter;
 
 public class ApplicationContextTest {
 	
@@ -34,6 +36,23 @@ public class ApplicationContextTest {
 		
 	}
 	
+	@Test
+	public void registerBeanWithDependency() {
+		StaticApplicationContext ac = new StaticApplicationContext();
+		
+		ac.registerBeanDefinition("printer", new RootBeanDefinition(StringPrinter.class));
+		
+		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+		helloDef.getPropertyValues().addPropertyValue("printer", new RuntimeBeanReference("printer"));
+		
+		ac.registerBeanDefinition("hello", helloDef);
+		
+		Hello hello = ac.getBean("hello", Hello.class);
+		hello.print();
+		
+		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+	}
 
 	
 }
