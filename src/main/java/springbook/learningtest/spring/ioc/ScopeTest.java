14486023c83c91b1cfbf3fd5ac7cfe91c80f5492
kimmari10 <kimmari10@naver.com>
2014-12-09 12:08:04 +0900

    prototypeScope 테스트

--- src/main/java/springbook/learningtest/spring/ioc/ScopeTest.java
+++ src/main/java/springbook/learningtest/spring/ioc/ScopeTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 public class ScopeTest {
 	
@@ -38,6 +39,34 @@ public class ScopeTest {
 	static class SingletonClientBean {
 		@Autowired SingletonBean bean1;
 		@Autowired SingletonBean bean2;
+	}
+	
+	@Test
+	public void prototypeScope() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(
+				PrototypeClientBean.class, PrototypeBean.class);
+		
+		Set<PrototypeBean> bean = new HashSet<PrototypeBean>();
+		
+		bean.add(ac.getBean(PrototypeBean.class));
+		assertThat(bean.size(), is(1));
+
+		bean.add(ac.getBean(PrototypeBean.class));
+		assertThat(bean.size(), is(2));
+
+		bean.add(ac.getBean(PrototypeBean.class));
+		assertThat(bean.size(), is(3));
+		
+		bean.add(ac.getBean(PrototypeBean.class));
+		assertThat(bean.size(), is(4));
+	}
+	
+	@Scope("prototype")
+	static class PrototypeBean {}
+	
+	static class PrototypeClientBean {
+		@Autowired PrototypeBean bean1;
+		@Autowired PrototypeBean bean2;
 	}
 
 }
